3b67f9018a4b9e77adf736b3cf1b6aedb04c1cc9
Frederik Creemers <frederikcreemers@gmail.com>
2018-05-31 05:04:23 +0200

    Update Channel interface to provide context to event listeners.

--- channels/types.go
+++ channels/types.go
@@ -1,6 +1,9 @@
 package channels
 
-import "net/http"
+import (
+	"net/http"
+	"context"
+)
 
 type Channel interface{
 	Namespace() string
@@ -12,8 +15,8 @@ type Channel interface{
 
 type ChannelProvider interface {
 	NewChannel(namespace, id string) Channel
-	OnJoin(namespace string, handler func(channel Channel, userId, socketId string))
-	OnLeave(namespace string, handler func(channel Channel, userId, socketId string))
+	OnJoin(ctx context.Context, namespace string, handler func(channel Channel, userId, socketId string))
+	OnLeave(ctx context.Context, namespace string, handler func(channel Channel, userId, socketId string))
 
 	HandleChannelAuth(w http.ResponseWriter, r *http.Request)
 	HadleWebHook(w http.ResponseWriter, r *http.Request)
