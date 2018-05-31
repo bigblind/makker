b1d3e3cbd54630f78da52fdbecb589c7a9b55ff6
Frederik Creemers <frederikcreemers@gmail.com>
2018-05-31 14:57:22 +0200

    Add /games/instances/{instanceId}/moves endpoint.

--- games/handlers.go
+++ games/handlers.go
@@ -7,6 +7,8 @@ import (
 	"github.com/gorilla/mux"
 	"github.com/bigblind/makker/handler_helpers"
 	"github.com/bigblind/makker/channels"
+	"encoding/json"
+	"io/ioutil"
 )
 
 func CreateInstace(w http.ResponseWriter, r *http.Request) {
@@ -34,6 +36,41 @@ func GetInstance(w http.ResponseWriter, r *http.Request)  {
 
 		vars := mux.Vars(r)
 		inst, err := inter.GetInstance(vars["instanceId"])
+		if err != nil {
+			handler_helpers.RespondWithJSONError(w, 400, err)
+		} else {
+			handler_helpers.RespondWithJSON(w, 200, instancetoResponse(&inst, r, cp))
+		}
+	})
+	if err != nil {
+		handler_helpers.RespondWithJSONError(w, http.StatusInternalServerError, err)
+	}
+}
+
+type MoveRequest struct {
+	Move interface{}
+}
+
+func MakeMove(w http.ResponseWriter, r *http.Request) {
+	err := di.Graph.Invoke(func(constructor StoreConstructor, cp channels.ChannelProvider) {
+		inter := NewInteractor(r.Context())
+
+		uid := users.GetUserId(r)
+
+		body, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			handler_helpers.RespondWithJSONError(w, http.StatusBadRequest, err)
+			return
+		}
+
+		var mr MoveRequest
+		err = json.Unmarshal(body, &mr)
+		if err != nil {
+			handler_helpers.RespondWithJSONError(w, http.StatusBadRequest, err)
+		}
+
+		vars := mux.Vars(r)
+		inst, err := inter.MakeMove(vars["instanceId"], uid, mr.Move)
 		if err != nil {
 			handler_helpers.RespondWithJSONError(w, 400, err)
 		} else {
