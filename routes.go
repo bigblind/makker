3e4b3753fd1e6d52e2e9da8a08d8ac09b09c02c9
Frederik Creemers <frederikcreemers@gmail.com>
2018-05-31 14:12:04 +0200

    set up http request handlers for channels

--- routes.go
+++ routes.go
@@ -4,6 +4,8 @@ import (
 	"github.com/gorilla/mux"
 	"github.com/bigblind/makker/users"
 	"github.com/bigblind/makker/games"
+	"github.com/bigblind/makker/di"
+	"github.com/bigblind/makker/channels"
 )
 
 func GetRouter() *mux.Router {
@@ -13,6 +15,11 @@ func GetRouter() *mux.Router {
 
 	r.HandleFunc("/games/{game}/new", games.CreateInstace).Methods("POST")
 	r.HandleFunc("/games/instances/{instanceId}", games.GetInstance).Methods("GET")
+
+	di.Graph.Invoke(func(cp channels.ChannelProvider) {
+		r.HandleFunc("/channels/auth", cp.HandleChannelAuth).Methods("POST")
+		r.HandleFunc("/channels/webhook", cp.HadleWebHook).Methods("POST")
+	})
 
 	r.Use(users.UserIdMiddleware)
 
