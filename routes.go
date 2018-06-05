3e4b60b07d477c2142d0bc81e8c70783efc8af55
Frederik Creemers <frederikcreemers@gmail.com>
2018-06-05 22:27:39 +0200

    Hook up games/move route to the correct handler

--- routes.go
+++ routes.go
@@ -16,7 +16,7 @@ func GetRouter() *mux.Router {
 	r.HandleFunc("/games/{game}/new", games.CreateInstace).Methods("POST")
 	r.HandleFunc("/games/instances/{instanceId}", games.GetInstance).Methods("GET")
 	r.HandleFunc("/games/instances/{instanceId}/start", games.StartGame).Methods("POST")
-	r.HandleFunc("/games/instances/{instanceId}/moves", games.GetInstance).Methods("POST")
+	r.HandleFunc("/games/instances/{instanceId}/moves", games.MakeMove).Methods("POST")
 
 	di.Graph.Invoke(func(cp channels.ChannelProvider) {
 		r.HandleFunc("/channels/auth", cp.HandleChannelAuth).Methods("POST")
