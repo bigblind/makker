d3240896f406576a3fadb6a5646d8760b319103b
Frederik Creemers <frederikcreemers@gmail.com>
2018-06-05 22:27:22 +0200

    Remove no longed needed di.Graph.Invoke calls in handlers

--- games/handlers.go
+++ games/handlers.go
@@ -2,8 +2,6 @@ package games
 
 import (
 	"encoding/json"
-	"github.com/bigblind/makker/channels"
-	"github.com/bigblind/makker/di"
 	"github.com/bigblind/makker/handler_helpers"
 	"github.com/bigblind/makker/users"
 	"github.com/gorilla/mux"
@@ -12,38 +10,28 @@ import (
 )
 
 func CreateInstace(w http.ResponseWriter, r *http.Request) {
-	err := di.Graph.Invoke(func(constructor StoreConstructor, cp channels.ChannelProvider) {
-		inter := NewInteractor(r.Context())
+	inter := NewInteractor(r.Context())
 
-		uid := users.GetUserId(r)
+	uid := users.GetUserId(r)
 
-		vars := mux.Vars(r)
-		inst, err := inter.CreateInstance(vars["game"], uid)
-		if err != nil {
-			handler_helpers.RespondWithJSONError(w, 400, err)
-		} else {
-			handler_helpers.RespondWithJSON(w, 200, inst)
-		}
-	})
+	vars := mux.Vars(r)
+	inst, err := inter.CreateInstance(vars["game"], uid)
 	if err != nil {
-		handler_helpers.RespondWithJSONError(w, http.StatusInternalServerError, err)
+		handler_helpers.RespondWithJSONError(w, 400, err)
+	} else {
+		handler_helpers.RespondWithJSON(w, 200, inst)
 	}
 }
 
 func GetInstance(w http.ResponseWriter, r *http.Request) {
-	err := di.Graph.Invoke(func(cp channels.ChannelProvider) {
-		inter := NewInteractor(r.Context())
+	inter := NewInteractor(r.Context())
 
-		vars := mux.Vars(r)
-		inst, err := inter.GetInstance(vars["instanceId"])
-		if err != nil {
-			handler_helpers.RespondWithJSONError(w, 400, err)
-		} else {
-			handler_helpers.RespondWithJSON(w, 200, inst)
-		}
-	})
+	vars := mux.Vars(r)
+	inst, err := inter.GetInstance(vars["instanceId"])
 	if err != nil {
-		handler_helpers.RespondWithJSONError(w, http.StatusInternalServerError, err)
+		handler_helpers.RespondWithJSONError(w, 400, err)
+	} else {
+		handler_helpers.RespondWithJSON(w, 200, inst)
 	}
 }
 
@@ -68,33 +56,27 @@ type MoveRequest struct {
 }
 
 func MakeMove(w http.ResponseWriter, r *http.Request) {
-	err := di.Graph.Invoke(func(constructor StoreConstructor, cp channels.ChannelProvider) {
-		inter := NewInteractor(r.Context())
+	inter := NewInteractor(r.Context())
 
-		uid := users.GetUserId(r)
+	uid := users.GetUserId(r)
 
-		body, err := ioutil.ReadAll(r.Body)
-		if err != nil {
-			handler_helpers.RespondWithJSONError(w, http.StatusBadRequest, err)
-			return
-		}
-
-		var mr MoveRequest
-		err = json.Unmarshal(body, &mr)
-		if err != nil {
-			handler_helpers.RespondWithJSONError(w, http.StatusBadRequest, err)
-		}
-
-		vars := mux.Vars(r)
-		inst, err := inter.MakeMove(vars["instanceId"], uid, mr.Move)
-		if err != nil {
-			handler_helpers.RespondWithJSONError(w, 400, err)
-		} else {
-			handler_helpers.RespondWithJSON(w, 200, inst)
-		}
-	})
-
+	body, err := ioutil.ReadAll(r.Body)
 	if err != nil {
-		handler_helpers.RespondWithJSONError(w, http.StatusInternalServerError, err)
+		handler_helpers.RespondWithJSONError(w, http.StatusBadRequest, err)
+		return
+	}
+
+	var mr MoveRequest
+	err = json.Unmarshal(body, &mr)
+	if err != nil {
+		handler_helpers.RespondWithJSONError(w, http.StatusBadRequest, err)
+	}
+
+	vars := mux.Vars(r)
+	inst, err := inter.MakeMove(vars["instanceId"], uid, mr.Move)
+	if err != nil {
+		handler_helpers.RespondWithJSONError(w, 400, err)
+	} else {
+		handler_helpers.RespondWithJSON(w, 200, inst)
 	}
 }
