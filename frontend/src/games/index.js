0414d6a7eeb5a6a62c9582bed3028a67be305ec3
Frederik Creemers <frederikcreemers@gmail.com>
2018-06-08 03:37:49 +0200

    Create GamesList class

---
+++ frontend/src/games/index.js
@@ -0,0 +1,29 @@
+class GamesList {
+    constructor() {
+        this.games = {}
+
+        this.addGame("WordSplash", 1, "WordSplash")
+    }
+
+    addGame(name, version, runner) {
+        if (!this.games[name]) {
+            this.games[name] = {}
+        }
+
+        this.games[name][version] = runner;
+    }
+
+    getGame(name, version) {
+        return this.games[name][version];
+    }
+
+    listGames() {
+        return Object.keys(this.games);
+    }
+
+    loadGame(name, version){
+        return import(`./runners/${this.games[name][version]}`);
+    }
+}
+
+export default new GamesList()
