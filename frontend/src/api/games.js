06be34ba6b1f56475fe35a44970872db897a08b1
Frederik Creemers <frederikcreemers@gmail.com>
2018-06-11 13:26:40 +0200

    Add GamesAPI.getInstance method

--- frontend/src/api/games.js
+++ frontend/src/api/games.js
@@ -4,7 +4,8 @@ import GamesList from "../games/"
 export default class GamesAPI extends APIBase {
     constructor() {
         super();
-        this.instances = {}; // maps from a list of games to a list of instances
+        this.instancesByGame = {}; // maps from a list of games to a list of instance IDs
+        this.instances = {}; // maps from instance ids to instances
         this.init();
     }
 
@@ -12,7 +13,14 @@ export default class GamesAPI extends APIBase {
         GamesList.listGames().forEach((g) => {
             GamesAPI.makeRequest("GET", `/games/${g}/instances`).then((instances) => {
                 console.log("loaded instances for", g, instances);
-                this.instances[g] = instances;
+
+                let ids = instances.map((i) => {
+                    this.instances[i.id] = i;
+                    return i.id;
+                });
+
+                this.instancesByGame[g] = ids;
+
                 this.emit("instances", {
                     game: g,
                     instances
@@ -22,6 +30,24 @@ export default class GamesAPI extends APIBase {
     }
 
     getInstances(game){
-        return this.instances[game] || [];
+        let ids = this.instances[game] || [];
+        return ids.map((id) => {
+            return this.instances[id];
+        })
+    }
+
+    getInstance(id) {
+        return this.instances[id]
+    }
+
+    refreshInstance(id) {
+        return GamesAPI.makeRequest("GET", `/games/instances/${id}`).then((instance) => {
+            let game = instance.game_info.name;
+            this.instances[id] = instance;
+            this.emit("instances", {
+                game,
+                instances: this.getInstances(game)
+            })
+        })
     }
 }
